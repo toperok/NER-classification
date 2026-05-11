2582b7afdd021fcb821238e8ae4d073e7cfb1970
Daniil Malashenko <daniil2007mmm@gmail.com>
2026-05-11 23:53:36 +0300

    many tools for working with data

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -1,3 +1,6 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer, DataCollatorForTokenClassification
 import datasets
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +10,8 @@ import pandas as pd
 import seaborn as sns
 import phik
 from phik import report
+
+
 
 def plot_phik_matrix(dataset, classes):
 
@@ -26,6 +31,7 @@ def plot_phik_matrix(dataset, classes):
     plt.title("Correlation matrix CLS")
     plt.show()
     plt.close()
+
 
 def eda(
     dataset: datasets.arrow_dataset.Dataset,
@@ -75,3 +81,101 @@ def eda(
     plt.close()
 
     plot_phik_matrix(dataset, classes)
+    
+    
+def create_label_mapping(dataset):
+    
+    unique_labels = set()
+    for example in dataset:
+        unique_labels.update(example["tags"])
+    unique_labels.add("O")
+    
+    label_list = sorted(list(unique_labels))
+    label2id = {label: i for i, label in enumerate(label_list)}
+    id2label = {i: label for label, i in label2id.items()}
+    
+    return label2id, id2label
+    
+
+def tokenize_and_align_labels(examples, tokenizer, label2id, max_length=512):
+    tokenized_inputs = tokenizer(
+        examples["tokens"],
+        truncation=True,
+        max_length=max_length,
+        padding=False, # Будем использовать паддинг в DataCollatorForTokenClassification
+        is_split_into_words=True,
+        return_offsets_mapping=True
+    )
+    
+    labels = []
+    for i, tags in enumerate(examples['tags']):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        prev_word_idx = None
+        label_ids = []
+        
+        for word_idx in word_ids:
+            if word_idx is None:
+                label_ids.append(-100)
+            elif word_idx != prev_word_idx:
+                label_ids.append(label2id[tags[word_idx]])
+            else:
+                label_ids.append(-100)
+            prev_word_idx = word_idx
+        labels.append(label_ids)
+    
+    tokenized_inputs["labels"] = labels
+    tokenized_inputs["cls_labels"] = examples["cls_vec"]
+    tokenized_inputs.pop("offset_mapping") 
+    
+    return tokenized_inputs
+
+
+class JointNERDataset(Dataset):
+    def __init__(self, tokenized_data):
+        self.data = tokenized_data
+    
+    def __len__(self):
+        return len(self.data["input_ids"])
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class JointCollator(DataCollatorForTokenClassification):
+    
+    def __call__(self, features):
+        cls_labels = [f.pop("cls_labels") for f in features]
+        batch = super().__call__(features)
+        batch["cls_labels"] = torch.tensor(cls_labels, dtype=torch.float)
+        
+        return batch
+
+
+def sanity_check_batch(dataloader, tokenizer, id2label, num_examples=5):
+    batch = next(iter(dataloader))
+    
+    print(f"\n{'=' * 70}\n")
+    print("Размерности тензоров")
+    print(f"\n{'=' * 70}\n")
+    
+    print(f"Input ids shape: {batch['input_ids'].shape}")
+    print(f"Attention Mask: {batch['attention_mask'].shape}")
+    print(f"NER labels shape: {batch['labels'].shape}")
+    print(f"CLS labels shape: {batch['cls_labels'].shape}")
+    
+    print(f"\n{'=' * 70}\n")
+    
+    for i in range(num_examples):
+        print(f"\nПример {i+1}\n")
+        print(f"Вектор классификации: {batch['cls_labels'][i][:10].tolist()}")
+        
+        tokens = tokenizer.convert_ids_to_tokens(batch["input_ids"][i])
+        labels = batch["labels"][i].tolist()
+        
+        print(f"{'Token':<20} | {'Label ID':<10} | {'Tag'}")
+        for token, label_id in zip(tokens[:30], labels[:30]):
+            if token == tokenizer.pad_token:
+                break
+            
+            tag = id2label[label_id] if label_id != -100 else "IGNORE"
+            print(f"{token:<22} {label_id:<12} {tag}")
